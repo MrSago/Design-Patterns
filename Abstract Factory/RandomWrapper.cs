291f20bbf3f3598c5cb4fe7a505bbb3254a6fabb
Alexander <37180898+MrSago@users.noreply.github.com>
2021-07-27 13:38:50 +0300

    hmm

---
+++ Abstract Factory/RandomWrapper.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace Abstract_Factory
+{
+    public static class RandomWrapper
+    {
+        private static readonly int seed = Environment.TickCount;
+        public static Random random = new(seed);
+    }
+}
+
