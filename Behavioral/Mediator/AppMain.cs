d688ac3704ffe45168de238fd6ae27fd46408e14
Alexander <37180898+MrSago@users.noreply.github.com>
2021-08-11 15:27:48 +0300

    Mediator

---
+++ Behavioral/Mediator/AppMain.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Mediator
+{
+    static class AppMain
+    {
+        static void Main()
+        {
+            Component1 component1 = new();
+            Component2 component2 = new();
+            ConcreteMediator concreteMediator = new(component1, component2);
+
+            Console.WriteLine("Client triggers operation A");
+            component1.DoA();
+
+            Console.Write('\n');
+
+            Console.WriteLine("Client triggers operation D");
+            component2.DoD();
+
+            _ = Console.ReadKey(true);
+        }
+    }
+}
+
---
+++ Behavioral/Mediator/Components.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+
+namespace Mediator
+{
+    abstract class BaseComponent
+    {
+        public IMediator _mediator;
+        
+        public BaseComponent(IMediator mediator = null)
+        {
+            _mediator = mediator;
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+
+    class Component1 : BaseComponent
+    {
+        public void DoA()
+        {
+            Console.WriteLine("Component 1 does A");
+
+            _mediator.Notify(this, "A");
+        }
+
+        public void DoB()
+        {
+            Console.WriteLine("Component 1 does B");
+
+            _mediator.Notify(this, "B");
+        }
+    }
+
+    class Component2 : BaseComponent
+    {
+        public void DoC()
+        {
+            Console.WriteLine("Component 2 does C");
+
+            _mediator.Notify(this, "C");
+        }
+
+        public void DoD()
+        {
+            Console.WriteLine("Component 2 does D");
+
+            _mediator.Notify(this, "D");
+        }
+    }
+}
+
