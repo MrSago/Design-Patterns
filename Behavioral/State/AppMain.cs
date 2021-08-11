92d4868ff6b693382961d2d4ac23102340f4d48f
Alexander <37180898+MrSago@users.noreply.github.com>
2021-08-11 15:27:19 +0300

    State

---
+++ Behavioral/State/AppMain.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace State
+{
+    static class AppMain
+    {
+        static void Main(string[] args)
+        {
+            Context context = new(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+            _ = Console.ReadKey(true);
+        }
+    }
+}
+
---
+++ Behavioral/State/States.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+namespace State
+{
+    abstract class State
+    {
+        protected Context _context;
+
+        public void SetContext(Context context)
+        {
+            _context = context;
+        }
+
+        public abstract void Handle1();
+
+        public abstract void Handle2();
+    }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateA handles request1.");
+            Console.WriteLine("ConcreteStateA wants to change the state of the context.");
+            _context.TransitionTo(new ConcreteStateB());
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateA handles request2.");
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateB handles request1.");
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateB handles request2.");
+            Console.WriteLine("ConcreteStateB wants to change the state of the context.");
+            _context.TransitionTo(new ConcreteStateA());
+        }
+    }
+}
+
